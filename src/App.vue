9de4fac3ec388edfdc88404da9986b4d18e34e94
djirdehh <hassan.djirdeh@mail.utoronto.ca>
2017-07-05 19:38:54 -0400

    initial scaffold of vue cli

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="css">
+@import '../node_modules/bulma/css/bulma.css';
+
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #FFF;
+  height: 100%;
+
+  background: #333;
+  background: -webkit-linear-gradient(left top, #024, #402);
+  background: -moz-linear-gradient(bottom right, #024, #402);
+  background: linear-gradient(to bottom right, #024, #402);
+}
+</style>
